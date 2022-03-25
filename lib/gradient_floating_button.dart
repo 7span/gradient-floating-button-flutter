f42cdfaee7b2931b4650449e4a79f01942822a94
Milan Surelia <milan@7span.com>
2022-03-25 22:34:52 +0530

    Initial Commit

---
+++ lib/gradient_floating_button.dart
@@ -0,0 +1,59 @@
+library gradient_floating_button;
+
+import 'package:flutter/material.dart';
+
+class GradientFloatingButton {
+  Widget withLinearGradient(
+      {required Function() onTap,
+      required Widget iconWidget,
+      required List<Color> colors,
+      double buttonHeight = 50,
+      double buttonWidth = 50,
+      BoxShape buttonShape = BoxShape.circle,
+      AlignmentGeometry alignmentBegin = Alignment.bottomLeft,
+      AlignmentGeometry alignmentEnd = Alignment.topRight,
+      List<double>? stops}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: buttonHeight,
+        width: buttonWidth,
+        decoration: BoxDecoration(
+            shape: buttonShape,
+            gradient: LinearGradient(
+                begin: alignmentBegin,
+                end: alignmentEnd,
+                stops: stops,
+                colors: colors)),
+        child: iconWidget,
+      ),
+    );
+  }
+
+  Widget withRadialGradient(
+      {required Function() onTap,
+      required Widget iconWidget,
+      required List<Color> colors,
+      required double radius,
+      double buttonHeight = 50,
+      double buttonWidth = 50,
+      BoxShape buttonShape = BoxShape.circle,
+      AlignmentGeometry? focal,
+      double focalRadius = 0}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: buttonHeight,
+        width: buttonWidth,
+        decoration: BoxDecoration(
+            shape: buttonShape,
+            gradient: RadialGradient(
+                radius: radius,
+                focal: focal,
+                focalRadius: focalRadius,
+                colors: colors)),
+        child: iconWidget,
+      ),
+    );
+  }
+}
